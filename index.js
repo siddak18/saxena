9789b21676d80c756304fcba0c20925d5aadfffa
siddak <siddakrajpal14@gmail.com>
2023-10-01 13:09:35 +0530

    loreum

--- index.js
+++ index.js
@@ -6,29 +6,6 @@ const car13=document.getElementById("car13");
 const car14=document.getElementById("car14");
 const car15=document.getElementById("car15");
 const car16=document.getElementById("car16");
-// arrow.addEventListener('click',()=>{
-//      arrow.style.display="none";
-//      arrow2.style.display="flex";
-//      car11.style.display="none";
-//      car12.style.display="none";
-//      car13.style.display="none";
-//      car14.style.display="flex";
-//      car15.style.display="flex";
-//      car16.style.display="flex";
-// });
-
-// arrow2.addEventListener('click',()=>{
-//     arrow.style.display="flex";
-//     arrow2.style.display="none";
-//      car11.style.display="flex";
-//      car12.style.display="flex";
-//      car13.style.display="flex";
-//      car14.style.display="none";
-//      car15.style.display="none";
-//      car16.style.display="none";
-// });
-
-
 
 const hide=document.getElementById("moveall");
 const hide1=document.getElementById("arrowcon");
